bb241861da8f7446c4124ef92d06e3c6b7339877
meg768 <meg768@users.noreply.github.com>
2017-08-14 22:25:01 +0200

    -

--- matrix.js
+++ matrix.js
@@ -8,7 +8,6 @@ var sprintf  = require('yow/sprintf');
 var isObject = require('yow/is').isObject;
 var isString = require('yow/is').isString;
 var logs     = require('yow/logs');
-var Queue    = require('yow/queue');
 var Matrix   = require('hzeller-matrix');
 
 
@@ -17,9 +16,10 @@ var Matrix   = require('hzeller-matrix');
 var App = function(argv) {
 
 	var _this    = this;
-	var _queue   = new Queue(50);
+	var _queue   = [];
 	var _matrix  = undefined;
 	var _socket  = undefined;
+	var _busy    = false;
 
 	var argv = parseArgs();
 
@@ -30,9 +30,10 @@ var App = function(argv) {
 		args.usage('Usage: $0 [options]');
 		args.help('help').alias('help', 'h');
 
-		args.option('height', {alias:'H',        describe:'Height of RGB matrix', default:32});
-		args.option('width',  {alias:'W',        describe:'Width of RGB matrix', default:32});
-		args.option('service',  {alias:'s',      describe:'Name of service', default:'hzeller-matrix-32x32'});
+		args.option('height',   {alias:'H',      describe:'Height of RGB matrix', default:32});
+		args.option('width',    {alias:'W',      describe:'Width of RGB matrix', default:32});
+		args.option('service',  {alias:'s',      describe:'Name of service', default:'matrix-32x32'});
+		args.option('simulate', {alias:'m',      describe:'Not running on a Raspberry Pi?', default:false});
 
 		args.wrap(null);
 
@@ -43,49 +44,184 @@ var App = function(argv) {
 		return args.argv;
 	}
 
+	function runRain(options) {
+		return new Promise(function(resolve, reject) {
+			console.log('runRain:', JSON.stringify(options));
+			_matrix.runRain(options, resolve);
+		});
+	}
+	function runPerlin(options) {
+		return new Promise(function(resolve, reject) {
+			console.log('runPerlin:', JSON.stringify(options));
+			_matrix.runPerlin(options, resolve);
+		});
 
 
-	function enqueue(options, fn) {
+	}
+	function runAnimation(options) {
+		return new Promise(function(resolve, reject) {
 
+			try {
+				options.fileName = options.name;
+
+				// Generate a random one if not specified
+				if (options.fileName == undefined) {
+					var files = fs.readdirSync(sprintf('%s/animations/%dx%d', __dirname, argv.width, argv.height));
+					options.fileName = random(files);
+				}
+				else {
+					options.fileName = sprintf('%s.gif', options.fileName);
+				}
+
+				// Add path
+				options.fileName = sprintf('%s/animations/%dx%d/%s', __dirname, argv.width, argv.height, options.fileName);
+
+				console.log('runImage:', JSON.stringify(options));
+				_matrix.runAnimation(options.fileName, options, resolve);
+
+			}
+			catch(error) {
+				reject(error);
+
+			}
+		});
+
+	}
+
+	function runClock(options) {
+
+		return new Promise(function(resolve, reject) {
+
+			options.fileName = options.name;
+
+			// Generate a random one if not specified
+			if (options.fileName == undefined) {
+				var files = fs.readdirSync(sprintf('%s/images/%dx%d/clocks', __dirname, argv.width, argv.height));
+				options.fileName = random(files);
+			}
+			else {
+				options.fileName = sprintf('%s.png', options.fileName);
+			}
+
+			// Add path
+			options.fileName = sprintf('%s/images/%dx%d/clocks/%s', __dirname, argv.width, argv.height, options.fileName);
+
+			console.log('runClock:', JSON.stringify(options));
+			_matrix.runClock(options.fileName, options, resolve);
+		});
+
+	}
+
+	function runImage(options) {
+
+		return new Promise(function(resolve, reject) {
+
+
+			if (!options.id || options.id < 1 || options.id > 846)
+				options.id = 704;
+
+			options.image = sprintf('%s/images/%dx%d/emojis/%d.png', __dirname, argv.height, argv.height, options.id);
+
+			console.log('runImage:', JSON.stringify(options));
+			_matrix.runImage(options.image, options, resolve);
+		});
+
+	}
+
+	function runEmoji(options) {
+		return new Promise(function(resolve, reject) {
+
+			if (!options.id || options.id < 1 || options.id > 846)
+				options.id = 704;
+
+			options.image = sprintf('%s/images/%dx%d/emojis/%d.png', __dirname, argv.height, argv.height, options.id);
+
+			console.log('runImage:', JSON.stringify(options));
+			_matrix.runImage(options.image, options, resolve);
+		});
+
+	}
+
+	function runText(options) {
+
+		return new Promise(function(resolve, reject) {
+
+			if (isString(options.fontName))
+				options.fontName = sprintf('%s/fonts/%s.ttf', __dirname, options.fontName);
+
+			console.log('runText:', JSON.stringify(options));
+			_matrix.runText(options.text, options, resolve);
+		});
+
+	}
+
+
+	function dequeue() {
+		return new Promise(function(resolve, reject) {
+			if (_queue.length > 0 && !_busy) {
+
+				_busy = true;
+
+				var message = _queue.splice(0, 1)[0];
+
+				message.method(message.options == undefined ? {} : message.options).then(function() {
+					console.log('Dequeueing...');
+					return dequeue();
+				})
+				.then(function() {
+					resolve();
+				})
+				.catch(function(error) {
+					reject(error);
+				})
+				.then(function() {
+					_busy = false;
+					console.log('Entering idle mode');
+					_socket.emit('idle', {});
+			})
+
+			}
+			else {
+				resolve();
+			}
+
+		});
+	}
+
+	function enqueue(method, options) {
+
+		console.log('!!!!!');
 		if (options == undefined)
 			options = {};
 
-		if (options.priority == 'low' && _matrix.isRunning())
+		var message = {
+			method:method,
+			options:options
+		};
+		console.log(message);
+
+		if (options.priority == 'low' && _busy)
 			return;
 
-		function enqueue() {
-			if (options.priority == '!') {
-				_queue.queue([fn]);
-				_matrix.stop();
-			}
-			else if (options.priority == 'high') {
-				_queue.prequeue(fn);
-			}
-			else {
-				_queue.enqueue(fn);
-			}
+		if (options.priority == '!') {
+			_queue = [message];
+			_matrix.stop();
 		}
-
-		function dequeue() {
-			_queue.dequeue().then(function() {
-				console.log('Entering idle mode');
-				_socket.emit('idle', {});
-
-			})
-			.catch(function(error) {
-				console.log(error.stack);
-				_socket.emit('idle', {});
-			});
-
-		}
-
-		if (_queue.isEmpty()) {
-			enqueue();
-			dequeue();
+		else if (options.priority == 'high') {
+			_queue.unshift(message);
 		}
 		else {
-			enqueue();
+			_queue.push(message);
 		}
+
+		dequeue().then(function() {
+
+		})
+		.catch(function(error) {
+
+		})
+
+
 
 	}
 
@@ -128,7 +264,7 @@ var App = function(argv) {
 		logs.prefix();
 
 
-		_matrix = new Matrix({width:argv.width, height:argv.height});
+		_matrix = new Matrix({hardware:argv.simulate ? 'none' : 'pi', width:argv.width, height:argv.height});
 
 
 		displayIP().then(function() {
@@ -141,22 +277,7 @@ var App = function(argv) {
 			_socket.on('connect', function() {
 				console.log('Connected to socket server!');
 
-				var options = {
-					id:704
-				};
-
-				enqueue(options, function() {
-					return new Promise(function(resolve, reject) {
-
-						if (!options.id || options.id < 1 || options.id > 846)
-							options.id = 704;
-
-						options.image = sprintf('%s/images/%dx%d/emojis/%d.png', __dirname, argv.height, argv.height, options.id);
-
-						console.log('runImage:', JSON.stringify(options));
-						_matrix.runImage(options.image, options, resolve);
-					});
-				});
+				enqueue(runEmoji, {id:704});
 
 				_socket.emit('i-am-the-provider');
 			});
@@ -166,143 +287,44 @@ var App = function(argv) {
 			});
 
 			_socket.on('cancel', function(options, fn) {
-				_queue.clear();
+				_queue = [];
 				_matrix.stop();
 				fn({status:'OK'});
 			});
 
 			_socket.on('stop', function(options, fn) {
-				_queue.clear();
+				_queue = [];
 				_matrix.stop();
 				fn({status:'OK'});
 			});
 
 			_socket.on('text', function(options, fn) {
-				options = options || {};
-
-				enqueue(options, function() {
-					return new Promise(function(resolve, reject) {
-
-						if (options.fontName)
-							options.fontName = sprintf('%s/fonts/%s.ttf', __dirname, options.fontName);
-
-						console.log('runText:', JSON.stringify(options));
-						_matrix.runText(options.text, options, resolve);
-					});
-				})
-
+				enqueue(runText, options);
 				fn({status:'OK'});
 			});
 
 			_socket.on('animation', function(options, fn) {
-				options = options || {};
-
-				enqueue(options, function() {
-					return new Promise(function(resolve, reject) {
-
-						try {
-							options = options || {};
-
-							options.fileName = options.name;
-
-							// Generate a random one if not specified
-							if (options.fileName == undefined) {
-								var files = fs.readdirSync(sprintf('%s/animations/%dx%d', __dirname, argv.width, argv.height));
-								options.fileName = random(files);
-							}
-							else {
-								options.fileName = sprintf('%s.gif', options.fileName);
-							}
-
-							// Add path
-							options.fileName = sprintf('%s/animations/%dx%d/%s', __dirname, argv.width, argv.height, options.fileName);
-
-							console.log('runImage:', JSON.stringify(options));
-							_matrix.runAnimation(options.fileName, options, resolve);
-
-						}
-						catch(error) {
-							reject(error);
-
-						}
-					});
-				});
-
+				enqueue(runAnimation, options);
 				fn({status:'OK'});
 			});
 
 			_socket.on('clock', function(options, fn) {
-				options = options || {};
-
-				enqueue(options, function() {
-					return new Promise(function(resolve, reject) {
-
-						options.fileName = options.name;
-
-						// Generate a random one if not specified
-						if (options.fileName == undefined) {
-							var files = fs.readdirSync(sprintf('%s/images/%dx%d/clocks', __dirname, argv.width, argv.height));
-							options.fileName = random(files);
-						}
-						else {
-							options.fileName = sprintf('%s.png', options.fileName);
-						}
-
-						// Add path
-						options.fileName = sprintf('%s/images/%dx%d/clocks/%s', __dirname, argv.width, argv.height, options.fileName);
-
-						console.log('runClock:', JSON.stringify(options));
-						_matrix.runClock(options.fileName, options, resolve);
-					});
-				});
-
+				enqueue(runClock, options);
 				fn({status:'OK'});
 			});
 
 			_socket.on('emoji', function(options, fn) {
-				options = options || {};
-
-				enqueue(options, function() {
-					return new Promise(function(resolve, reject) {
-
-						if (!options.id || options.id < 1 || options.id > 846)
-							options.id = 704;
-
-						options.image = sprintf('%s/images/%dx%d/emojis/%d.png', __dirname, argv.height, argv.height, options.id);
-
-						console.log('runImage:', JSON.stringify(options));
-						_matrix.runImage(options.image, options, resolve);
-					});
-				});
-
+				enqueue(runEmoji, options);
 				fn({status:'OK'});
 			});
 
 			_socket.on('rain', function(options, fn) {
-				options = options || {};
-
-				enqueue(options, function() {
-					return new Promise(function(resolve, reject) {
-						console.log('runRain:', JSON.stringify(options));
-						_matrix.runRain(options, resolve);
-					});
-
-				});
-
+				enqueue(runRain, options);
 				fn({status:'OK'});
 			});
 
 			_socket.on('perlin', function(options, fn) {
-				options = options || {};
-
-				enqueue(options, function() {
-					return new Promise(function(resolve, reject) {
-						console.log('runPerlin:', JSON.stringify(options));
-						_matrix.runPerlin(options, resolve);
-					});
-
-				});
-
+				enqueue(runPerlin, options);
 				fn({status:'OK'});
 			});
 
